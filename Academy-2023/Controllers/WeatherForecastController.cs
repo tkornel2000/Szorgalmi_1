d3d2d46479510d05e1e353a5c0bb2747bf6fc380
Varga Oresztész <oresztesz.varga@attrecto.com>
2023-02-28 11:31:07 +0100

    Second lesson
    - Nuget package manager
    - CRUD
    - Model validation
    - Repository pattern

--- Academy-2023/Controllers/WeatherForecastController.cs
+++ Academy-2023/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Academy_2023.Controllers
 {
@@ -21,13 +22,17 @@ namespace Academy_2023.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var asd = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            var jsonAsd = JsonConvert.SerializeObject(asd);
+
+            return asd;
         }
     }
 }
---
+++ Academy-2023/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using Academy_2023.Data;
+
+namespace Academy_2023.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new List<User>();
+
+        public IEnumerable<User> GetAll()
+        {
+            return Users;
+        }
+
+        public User? GetById(int id)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public void Create(User data)
+        {
+            Users.Add(data);
+        }
+
+        public User? Update(int id, User data)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    user.FirstName = data.FirstName;
+                    user.LastName = data.LastName;
+
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    Users.Remove(user);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
