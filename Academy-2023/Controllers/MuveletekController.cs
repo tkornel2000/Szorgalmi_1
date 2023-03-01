d61b8b2ab3e2db410fa15a0223c3be64c215f3fe
tkornel2000 <tkornel.takacs2000@gmail.com>
2023-03-01 12:39:15 +0100

    Hozzáadtam a nuget package használatát

---
+++ Academy-2023/Controllers/MuveletekController.cs
@@ -0,0 +1,45 @@
+﻿using Academy_2023.Data;
+using AppLogger;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Academy_2023.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuveletekController : ControllerBase
+    {
+        private static Szamparos szamparos = new Szamparos(1, 1);
+        // GET: api/<OsszeadController>
+        [HttpGet("Osszead")]
+        public float Get()
+        {
+            return szamparos.osszeadas();
+        }
+        [HttpGet("Kivon")]
+        public float Get2()
+        {
+            return szamparos.kivonas();
+        }
+        [HttpGet("Szoroz")]
+        public float Get3()
+        {
+            return szamparos.szorzas();
+        }
+        [HttpGet("Oszt")]
+        public float Get4()
+        {
+            return szamparos.osztas();
+        }
+
+        // POST api/<OsszeadController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Ketszam ketszam)
+        {
+            szamparos.Setter(ketszam.szam1, ketszam.szam2);
+            return ModelState.IsValid?Ok():NoContent();
+        }
+
+    }
+}
---
+++ Academy-2023/Data/Ketszam.cs
@@ -0,0 +1,8 @@
+﻿namespace Academy_2023.Data
+{
+    public class Ketszam
+    {
+        public float szam1 { get; set; }
+        public float szam2 { get; set; }
+    }
+}
